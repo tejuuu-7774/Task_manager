e737eaffd0b9165621987bd37d286da2c1df725c
Palwai Tejaswini <palwaitejaswini7777@gmail.com>
2026-02-05 23:01:56 +0530

    Main model added.

---
+++ backend/models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+// Main Task Schema 
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
